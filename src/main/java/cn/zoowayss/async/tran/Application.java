bf48105e1898fe894bd466b97f0ce365c346f638
Zoowayss <shizuwei99@gmail.com>
2023-08-05 09:48:51 +0800

    feat: start project

---
+++ src/main/java/cn/zoowayss/async/tran/Application.java
@@ -0,0 +1,16 @@
+package cn.zoowayss.async.tran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author: <a href="https://github.com/zooways">zooways</a>
+ * @Date: 2023/7/28 14:03
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
